8c1da9acd61fa324be9edf84741feaf0f1cc6a1a
Dantallope <dantebraccialini@gmail.com>
2023-03-02 19:40:41 -0700

    heroku prep

--- server.js
+++ server.js
@@ -1,29 +1,10 @@
-require("dotenv").config();
+const express = require('express')
 const path = require('path')
-const express = require('express');
-const exphbs = require('express-handlebars');
-const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 5000
 
-const sequelize = require('./config/connection')
-const mainRouter = require("./controllers")
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(mainRouter);
-
-sequalize.sync().then(() =>{
-  app.listen(PORT, () => { console.log('now listening')
-});
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
